f3cc5676c6461c43119d62d08aa82ff6f362d797
hakim-nemar <hakim.nemar@epitech.eu>
2022-12-30 23:07:55 +0100

    mise en place front du header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import loupe from '../../assets/img/loupe.svg';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <header>Header</header>
+        <header>
+            <div className='head'>
+                <Link to="/"><img src={Logo} alt='Logo' /></Link>
+
+                <ul>
+                    <li>
+                        <Link to="/new">Nouveautés</Link>
+                    </li>
+                    <li>
+                        <Link to="/new">Populaire</Link>
+                    </li>
+                    <li>
+                        <Link to="/new">Sports</Link>
+                    </li>
+                    <li>
+                        <img src={loupe} className="loupe" alt="loupe" />
+                        <input type='search' id='search' name='search' placeholder='Rechercher un film ou une série' />
+                    </li>
+                    <li>
+                        <button>Se connecter</button>
+                    </li>
+                </ul>
+            </div>
+        </header>
     )
 }
